2da957a76160acb688e5588018715d0632e18e27
mascaryu <mascarenhasyuri@hotmail.com>
2024-02-16 16:00:30 -0300

    [Add] plane with text texture

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 const height = 600; // Height, in pixels, of the poster sketch
-const detailInterval = 2000; // Time interval for detail update (2 seconds)
+const detailInterval = 5000; // Time interval for detail update (2 seconds)
 let detailCounter = 3; // Initial detail. Both X and Y will have equal detail level
 let lastUpdate = 0; // Timestamp of last detail update
+let tb; // Text image buffer to be used in main canvas
 
 const checkDetail = () => {
   if (millis() - lastUpdate < detailInterval) return;
@@ -15,10 +16,18 @@ const checkDetail = () => {
 
 function setup() {
   createCanvas(height * (18 / 24), height, WEBGL);
+
+  tb = createGraphics(height * (18 / 24), height);
+  tb.textSize(32);
+  tb.textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(200);
+
+  //   tb.background();
+  tb.fill(0);
+  tb.text("Is it really you?", width / 2, height / 2);
 
   push();
   noFill();
@@ -27,5 +36,11 @@ function draw() {
   stroke(20);
   checkDetail();
   sphere(100, detailCounter, detailCounter);
+  pop();
+
+  push();
+  texture(tb);
+  noStroke();
+  plane(width / 2, height / 2);
   pop();
 }
