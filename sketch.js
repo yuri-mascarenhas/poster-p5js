9be51b5c44446ecd611bd92f4016b505c2c215fa
mascaryu <mascarenhasyuri@hotmail.com>
2024-02-16 09:48:29 -0300

    [Add] Dynamic detail updater for sphere

--- sketch.js
+++ sketch.js
@@ -1,4 +1,17 @@
-const height = 600;
+const height = 600; // Height, in pixels, of the poster sketch
+const detailInterval = 2000; // Time interval for detail update (2 seconds)
+let detailCounter = 3; // Initial detail. Both X and Y will have equal detail level
+let lastUpdate = 0; // Timestamp of last detail update
+
+const checkDetail = () => {
+  if (millis() - lastUpdate < detailInterval) return;
+  lastUpdate = millis();
+  if (detailCounter >= 16) {
+    detailCounter = 3;
+    return;
+  }
+  detailCounter += 2;
+};
 
 function setup() {
   createCanvas(height * (18 / 24), height, WEBGL);
@@ -10,7 +23,9 @@ function draw() {
   push();
   noFill();
   rotateY(millis() / 10000);
+  rotateZ(millis() / 10000);
   stroke(20);
-  sphere(100);
+  checkDetail();
+  sphere(100, detailCounter, detailCounter);
   pop();
 }
